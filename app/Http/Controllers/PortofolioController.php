cca1cc37513f85c025de6d78b2758f3c4a79d374
reginafldaa <regina.dalimunthe@gmail.com>
2026-01-28 15:20:08 +0700

    new updates index

--- app/Http/Controllers/PortofolioController.php
+++ app/Http/Controllers/PortofolioController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Portofolio;
 
 class PortofolioController extends Controller
 {
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/portofolios', [App\Http\Controllers\PortofolioController::class, 'index'])->name('api.portofolios.index');
